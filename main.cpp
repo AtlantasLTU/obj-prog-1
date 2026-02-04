db18e95bdcf4ad246710b5464c2cb0bed80cd5dc
Atlantas Ališauskas <atlantasalisauskas2014@gmail.com>
2026-02-04 13:18:16 +0200

    Nuskaitoma vieno studento informacija bei rezultatai iš vartotojo įvesties, ruošiama išvestis

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+using std::vector;
+using std::cin;
+using std::cout;
+
+std::ifstream in("duom.txt");
+std::ofstream out("rez.txt");
+
+struct Studentas{
+    std::string vardas = "R", pavarde = "R";
+    //int *paz; // nuspresti kiek pazymiu tures studentas galu gale
+    vector<int> paz;
+    int rez;
+};
+
+int main(){
+    Studentas A;
+    cin >> A.vardas >> A.pavarde;
+    for(int i = 0; i < 10; i++){
+        int t;
+        cin >> t;
+        A.paz.insert(A.paz.begin(), t);
+    }
+    //A.rez = A.paz.
+
+    cout << "Pavarde" << std::setw(10) <<  "Vardas" << std::setw(40) << "Galutinis (Vid.) / Galutinis (Med.)" << std::endl;
+    cout << std::setfill('-') << std::setw(59) << "-" << std::endl;
+
+    return 0;
+}
