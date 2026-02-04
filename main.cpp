a4ce863596c4af86f04daffd6e93a161578271c2
Atlantas Ališauskas <atlantasalisauskas2014@gmail.com>
2026-02-04 13:31:05 +0200

    Pridėtas pasirinkimas medianoms, tačiau funkcionalumas dar neimplementuotas

--- main.cpp
+++ main.cpp
@@ -17,6 +17,17 @@ struct Studentas{
 };
 
 int main(){
+    bool medianos;
+    char t;
+    cout << "Išvesti medianas? (N, y)" << std::endl;
+    while(t!='N' || t!='Y' || t!='n' || t!='y'){
+        cin >> t;
+    }
+    if(t == 'Y' || t == 'y'){
+        medianos = true;
+    } else if(t == 'N' || t == 'n'){
+        medianos = false;
+    }
     Studentas A;
     cin >> A.vardas >> A.pavarde;
     for(int i = 0; i < 10; i++){
@@ -28,6 +39,9 @@ int main(){
 
     cout << "Pavarde" << std::setw(10) <<  "Vardas" << std::setw(40) << "Galutinis (Vid.) / Galutinis (Med.)" << std::endl;
     cout << std::setfill('-') << std::setw(59) << "-" << std::endl;
+    for(int i = 0; i < 2; i++){
+        cout << A
+    }
 
     return 0;
 }
