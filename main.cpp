57251a8b84df84c2bb85ec000606fbe9a8eaf98d
Atlantas Ališauskas <50662924+AtlantasLTU@users.noreply.github.com>
2026-02-08 22:52:37 +0200

    Perdaryta rezultatų įvestis, paruošta ateitį reikalingui dinamiškumui.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ using std::cin;
 using std::cout;
 
 #ifdef _WIN32 // naudojame preprocesorių, kad kompiliatorius, naudojant Windows, pridėtų windows.h antraščių failą, kad vėliau galėtume pakeistį terminalo išvesties ir įvesties užkodavimą į UTF-8
-#include <windows.h>
+#include <windows.h> // windows antraščių failas
 #endif
+
 // input tikrinimas
 // galutinio rez skaiciavimas
 // ivedimo patikslinimas
@@ -25,29 +26,39 @@ struct Studentas{
 };
 
 int main(){
-    #ifdef _WIN32
+
+    #ifdef _WIN32 // Jei kompiliuojama Windows operacinei sistemai nustatyti konsoles įvestį ir išvestį UTF-8 užkodavimui.
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     #endif
+
     bool medianos;
     char t;
     cout << "Išvesti medianas? (N, y)" << std::endl;
-    while(t!='N' && t!='Y' && t!='n' && t!='y'){
+    do {
         cin >> t;
-    }
+    } while(t!='N' && t!='Y' && t!='n' && t!='y');
     if(t == 'Y' || t == 'y'){
         medianos = true;
     } else if(t == 'N' || t == 'n'){
         medianos = false;
     }
+
+    cout << "Įveskite studento vardą bei pavardę" << std::endl;
     Studentas A;
     cin >> A.vardas >> A.pavarde;
-    for(int i = 0; i < 10; i++){
-        cout << "Įveskite namų darbų pažymį" << std::endl;
-        int t;
-        cin >> t;
-        A.nd.push_back(t);
-    }
+    
+    int ndPaz;
+    cout << "Įveskite namų darbų pažymį: ";
+    cin >> ndPaz;
+    do{
+        A.nd.push_back(ndPaz);
+        cout << A.nd.size() << " " << std::endl;
+        cout << "Įveskite namų darbų pažymį: ";
+    }while(cin >> ndPaz && A.nd.size() < 9);
+    cout << "Įveskite egzamino rezultatą: ";
+    cin >> A.rez;
+    cout << std::endl;
     //A.rez = A.paz.
 
     cout << "Pavarde" << std::setw(10) <<  "Vardas" << std::setw(40) << "Galutinis (Vid.) / Galutinis (Med.)" << std::endl;
